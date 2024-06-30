6b34040532657cc560c989748e62be02eeebcbcc
garciaa38 <garciaa38@gmail.com>
2024-06-30 13:48:40 -0400

    Fix bugs with classes and students

--- react-vite/src/components/AddStudentModal/AddStudentModal.jsx
+++ react-vite/src/components/AddStudentModal/AddStudentModal.jsx
@@ -7,12 +7,14 @@ function AddStudentModal({ cls }) {
     const { id: classId } = cls;
     const [formAppear, setFormAppear] = useState(false)
     const allStudents = useSelector((state) => Object.values(state.students));
+    console.log("FETCH STUDENTS from store", allStudents)
+    const [allStudentsState, setAllStudentsState] = useState(allStudents)
 
     if (!formAppear) {
         return (
             <>
                 <h1>Add a Student here!</h1>
-                <StudentSearch allStudents={allStudents} classId={classId} />
+                <StudentSearch allStudents={allStudentsState} classId={classId} setAllStudentsState={setAllStudentsState} />
                 <h3>{`Don't see your student?`} <button onClick={() => setFormAppear(true)}>Sign them up ahead of time to get them started!</button></h3>
             </>
         );
@@ -20,7 +22,7 @@ function AddStudentModal({ cls }) {
         return (
             <>
                 <h1>Sign up your student below using their school email and password!</h1>
-                <StudentSignUp classId={classId} />
+                <StudentSignUp classId={classId} setAllStudentsState={setAllStudentsState}/>
             </>
         )
     }
